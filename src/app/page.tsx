6d7e655e3e5b0a1597bf14d01e3375d45eb02b46
Gustavo Gebhardt <gustavogebhardt@MacBook-Air-de-Gustavo.local>
2024-04-02 20:57:24 -0300

    inicio do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,37 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hello, World!</h1>
-    </main>
+    <div>
+      <div className={styles.divTela1}>
+        <img className={styles.imgLogoTexto} src="/logo-texto.png"></img>
+        <div className={styles.divTexto}>
+          <p className={styles.text}>
+            Lorem ipsum phasellus nisl quisque<br></br>
+            senectus hac ut euismod bibendum<br></br>
+            habitant erat aliquam habitant.<br></br>
+            nulla volutpat quisque pharetra<br></br>
+            magna inceptos mauris suscipit,<br></br>
+            ipsum leo etiam himenaeos
+          </p>
+        </div>
+        <img src="seta.png" className={styles.imgSeta}></img>
+      </div>
+      <div className={styles.divTela2}>
+        <div className={styles.container}>
+          <img className={styles.logo} src="logo.png"></img>
+          <p className={styles.text}>
+            Lorem ipsum phasellus nisl quisque<br></br>
+            senectus hac ut euismod bibendum<br></br>
+            habitant erat aliquam habitant.<br></br>
+            nulla volutpat quisque pharetra<br></br>
+            magna inceptos mauris suscipit,<br></br>
+            ipsum leo etiam himenaeos
+          </p>
+        </div>
+        <div className={styles.containerMaster}>
+
+        </div>
+      </div>
+    </div>
   );
 }
