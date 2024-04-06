54858c20e758ae06e627d5ae6fd929e3e054a34d
Gustavo Gebhardt <gustavogebhardt@MacBook-Air-de-Gustavo.local>
2024-04-06 17:28:47 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
         <a className={styles.input} href="#menuHeader"><img className={styles.imgHeader} src="bars-solid.svg"></img></a>
         <div className={styles.menuHeader} id="menuHeader">
           <a className={styles.inputFechar} href="/#"><img className={styles.imgHeader} src="xmark-solid.svg"></img></a>
-          <a className={styles.link}>Link</a>
-          <a className={styles.link}>Link</a>
-          <a className={styles.link}>Link</a>
-          <a className={styles.link}>Link</a>
+          <a className={styles.link} href="/">Inicio</a>
+          <a className={styles.link}>Programação</a>
+          <a className={styles.link}>Contato</a>
+          <a className={styles.link}>Sobre</a>
         </div>
       </header>
       <img className={styles.imgEstrela1} src="estrela.png"></img>
