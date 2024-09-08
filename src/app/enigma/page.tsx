088f753e2337f9e7f658d6882171a13b0d8adfa9
GustavoGebhardt <gustavorgebhardt@gmail.com>
2024-09-08 10:50:15 -0300

    Vazamento

--- src/app/enigma/page.tsx
+++ src/app/enigma/page.tsx
@@ -1,27 +1,14 @@
 "use client"
 
 import styles from "./page.module.css";
-import Cookie from "js-cookie";
-import { useRouter } from "next/navigation";
 
 export default function Enigma() {
 
-  const router = useRouter()
-
-  async function handleLogin(){
-    fetch("https://clube-backend.onrender.com/users")
-    Cookie.set("token_level", "level1")
-    router.push("/enigma/level1")
-  }
-
-  return(
-    <div>
-      <div className={styles.divBackground}>
-        <div className={styles.divInicio}>
-          <p className={styles.text}>Olá.<br></br><br></br> Nos voltamos!<br></br><br></br>Esperamos você do outro lado.<br></br><br></br> Boa sorte!<br></br><br></br> - C</p>
-          <button className={styles.btn} onClick={handleLogin}>Iniciar</button>
-        </div>
+  return (
+    <>
+      <div className={styles.divMaster}>
+        <p className={styles.text}>Não vou conseguir descansar até descobrir o que aconteceu com eles</p>
       </div>
-    </div>
+    </>
   );
 }
